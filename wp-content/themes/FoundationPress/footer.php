d99e8b4d70af4fb791a2ed851e239dd33686a415
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-11 12:29:51 -0700

    foundation build

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -105,9 +105,5 @@
     }
 </style>
 
-<script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.5'><\/script>".replace("HOST", location.hostname));
-//]]></script>
-
 </body>
 </html>
