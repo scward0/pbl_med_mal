81513c756f7fa55cb0058a749d9944459042bd1d
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-16 16:15:16 -0700

    anchor foot again

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -1,7 +1,7 @@
-<footer style="background: url('http://198.58.102.147/pbl_med_mal/wp-content/uploads/2017/01/Footer.jpg') no-repeat center bottom;">
+<footer style="background: url('http://198.58.102.147/pbl_med_mal/wp-content/uploads/2017/01/Footer2.jpg') no-repeat center bottom;">
   <div class="row" style="margin-top: 100px;">
     <div class="columns small-12 medium-6">
-      <img style="float: right;" src="http://198.58.102.147/pbl_med_mal/wp-content/uploads/2016/12/pbl_logo.png" class="pbl-logo" width="100px" alt="" />
+      <img style="float: right;" src="http://198.58.102.147/pbl_med_mal/wp-content/uploads/2016/12/pbl_logo.png" class="pbl-logo" width="100px" alt="PBL Logo" />
     </div>
     <div class="columns small-12 medium-6">
       <div class="contact-info" style="float: left">
