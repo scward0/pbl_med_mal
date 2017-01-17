bae3221b067e85184efc1025723b31b6bf139016
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-17 11:32:41 -0700

    wp login logo

--- wp-content/themes/FoundationPress/functions.php
+++ wp-content/themes/FoundationPress/functions.php
@@ -61,7 +61,7 @@ add_action('init', 'register_theme_menus');
 function my_login_logo() { ?>
     <style type="text/css">
         #login h1 a, .login h1 a {
-            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/charles_thronson_logo.png);
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/WP_Login_Logo.jpg);
             padding-bottom: 30px;
         }
     </style>
