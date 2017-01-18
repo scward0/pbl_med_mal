9de0abcb004178481a808e1491d298634b9fb33e
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-18 09:29:22 -0700

    disclaimer page

---
+++ wp-content/themes/FoundationPress/page-disclaimer.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+
+<div class="row" class="collapse hide-for-small-only" style="max-width: none;">
+  <div>
+    <img class="hero hide-for-small-only" src="<?php the_field('hero'); ?>" alt="hero image" style="width: 100%;" />
+  </div>
+</div>
+
+<div class="row" class="collapse show-for-small-only" style="max-width: none;">
+  <div>
+    <img class="hero show-for-small-only" src="<?php the_field('mobile_hero'); ?>" alt="mobile hero image" style="width: 100%;" />
+  </div>
+</div>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
+  <div class="container white">
+    <div class="row">
+      <div class="columns small-12 medium-12 large-12" id="about">
+        <h3 class="text-center"><?php the_title(); ?></h3>
+        <p class="body-copy">
+          <?php echo get_the_content(); ?>
+        </p>
+      </div>
+    </div>
+  </div>
+
+<?php endwhile; else : ?>
+
+<p><?php _e( 'Sorry, no pages found' ); ?></p>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
