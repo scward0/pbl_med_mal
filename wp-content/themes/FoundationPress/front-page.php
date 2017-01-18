fe9aa57aef0d28b556fce1fcd71089a6564bbbc6
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-18 10:12:44 -0700

    fix visibility class

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -1,5 +1,5 @@
 <div class="collapse" style="padding: 0;">
-  <div class="slick desk-slick hide-on-small-only">
+  <div class="slick desk-slick hide-for-small-only">
     <div style="background: url('http://198.58.102.147/pbl_med_mal/wp-content/uploads/2017/01/MedMal_HeaderArtboard_1.jpg'); height: 100%; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover; overflow: hidden; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover; ">
       <img style="display: none; height:100vh !important; width: 100%; overflow: scroll !important; -webkit-overflow-scrolling: touch !important;" alt="slick hidden" />
     </div>
@@ -13,7 +13,7 @@
       <img style="display: none; height:100vh !important; width: 100%; overflow: scroll !important; -webkit-overflow-scrolling: touch !important;" alt="slick hidden" />
     </div>
   </div>
-  <div class="slick mobile-slick show-on-small-only">
+  <div class="slick mobile-slick show-for-small-only">
     <div style="background: url(''); height: 100%; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover; overflow: hidden; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover; ">
       <img style="display: none; height:100vh !important; width: 100%; overflow: scroll !important; -webkit-overflow-scrolling: touch !important;" alt="slick hidden" />
     </div>
