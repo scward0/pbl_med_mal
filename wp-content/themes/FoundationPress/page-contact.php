166f4e737331b96791ccdc2ac490acb98b09ee23
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-18 11:01:11 -0700

    map

--- wp-content/themes/FoundationPress/page-contact.php
+++ wp-content/themes/FoundationPress/page-contact.php
@@ -37,19 +37,25 @@
         </style>
         <div id="map"></div>
         <script>
-          function initMap() {
-            var uluru = {lat: -25.363, lng: 131.044};
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 4,
-              center: uluru
-            });
-            var marker = new google.maps.Marker({
-              position: uluru,
-              map: map
-            });
+        function initMap() {
+              var myLatLng = {lat: -25.363, lng: 131.044};
+
+              var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 4,
+                center: myLatLng
+              });
+
+              var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: 'Hello World!'
+              });
+            }});
           }
         </script>
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsE-_-FVLPYMP2wBZCPWoLj9x3FSJNfUQ&callback=initMap"></script>
+        <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsE-_-FVLPYMP2wBZCPWoLj9x3FSJNfUQ&callback=initMap">
+        </script>
       </div>
     </div>
   </div>
