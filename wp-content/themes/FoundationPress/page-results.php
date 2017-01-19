009e077f538d2094989941b53bda74c98fe3a8e4
Sean Ward <seanward@Seans-MacBook-Pro.local>
2017-01-19 10:17:15 -0700

    new results page

---
+++ wp-content/themes/FoundationPress/page-results.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<div class="row" class="collapse" style="max-width: none;">
+  <div>
+    <img class="hero" src="<?php the_field('hero'); ?>" alt="hero image" style="width: 100%;" />
+  </div>
+</div>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
+  <div class="container white">
+    <div class="row">
+      <div class="columns small-12 medium-12 large-8" id="about">
+        <h3 class="text-center"><?php the_title(); ?></h3>
+        <p class="body-copy" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+            <?php echo get_field('content'); ?>
+        </p>
+      </div>
+      <div id="form" class="columns small-12 medium-12 large-4">
+          <?php the_field('ninja'); ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="columns small-12 medium-12 large-12">
+        <div class="embed-container" style="padding: 10%;">
+	         <?php the_field('video'); ?>
+         </div>
+      </div>
+    </div>
+  </div>
+
+<?php endwhile; else : ?>
+
+<p><?php _e( 'Sorry, no pages found' ); ?></p>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
