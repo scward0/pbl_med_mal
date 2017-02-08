4c6b55945d6832127ddf15276c6959d6c4dc66fd
Sean Ward <scward0@gmail.com>
2017-02-08 15:29:49 -0700

    webmaster

--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -14,6 +14,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="google-site-verification" content="ps30HK-GT-aEAcZ9rZBaH3KRmnLUDTNZY53Z84laqsA" />
 		<link rel="stylesheet" type="text/css" href="http://www.medicalmalpracticeattorneyinfo.com/wp-content/themes/FoundationPress/slick/slick.css"/>
 		<link rel="stylesheet" type="text/css" href="http://www.medicalmalpracticeattorneyinfo.com/wp-content/themes/FoundationPress/slick/slick-theme.css"/>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
