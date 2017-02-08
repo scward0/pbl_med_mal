d652a74448f175563fd93328791f1415651e43ea
Sean Ward <scward0@gmail.com>
2017-02-08 15:17:35 -0700

    center footer logo

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -1,6 +1,7 @@
 <footer style="background: url('http://www.medicalmalpracticeattorneyinfo.com/wp-content/uploads/2017/01/footer3.jpg') no-repeat center bottom; background-size: cover;">
   <div class="row" style="padding-top: 150px;">
     <div class="columns small-12 medium-6">
+      <br>
       <img style="float: right;" src="http://www.medicalmalpracticeattorneyinfo.com/wp-content/uploads/2016/12/pbl_logo.png" class="pbl-logo" width="100px" alt="PBL Logo" />
     </div>
     <div class="columns small-12 medium-6">
