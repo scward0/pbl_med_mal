284a87ee345163ff4ea6ea9e096abd87bc954e17
Sean Ward <scward0@gmail.com>
2017-08-30 15:47:02 -0600

    thank you

--- wp-content/themes/FoundationPress/page-thank-you.php
+++ wp-content/themes/FoundationPress/page-thank-you.php
@@ -1,15 +1,30 @@
-<div class="container white">
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <?php get_header(); ?>
-  <div class="row">
-    <div class="columns small-12 medium-12 large-8" id="about">
-      <h3 class="text-center secondary-title">Thank you for your submission</h3>
-      <p style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 20px; margin-top: 50px;">
-      Your case information is very valuable to us, we will contact you regarding your case as soon as we can.
-      </p>
-    </div>
+<?php get_header(); ?>
+
+<div class="row" class="collapse hide-for-small-only" style="max-width: none;">
+  <div>
+    <img class="hero hide-for-small-only" src="<?php the_field('hero'); ?>" alt="hero image" style="width: 100%;" />
   </div>
 </div>
+
+<div class="row" class="collapse show-for-small-only" style="max-width: none;">
+  <div>
+    <img class="hero show-for-small-only" src="<?php the_field('mobile_hero'); ?>" alt="mobile hero image" style="width: 100%;" />
+  </div>
+</div>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+
+  <div class="container white">
+    <div class="row">
+      <div class="columns small-12 medium-12 large-12" id="about">
+        <h3 class="text-center secondary-title">Thank you for your submission</h3>
+        <p style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 20px; margin-top: 50px;">
+          Your case information is very valuable to us, we will contact you regarding your case as soon as we can.
+        </p>
+      </div>
+    </div>
+  </div>
 
 <?php endwhile; else : ?>
 
